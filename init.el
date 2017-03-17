642323908334e195d0bbcb09477c131dd82c48c7
zhexuany <zhexuany@gmail.com>
2017-03-17 08:49:56 +0000

    added magithub and editorconfig

--- init.el
+++ init.el
@@ -7,6 +7,7 @@
    dotspacemacs-configuration-layer-path '()
    dotspacemacs-configuration-layers
    '(
+     nginx
      html
      yaml
      ;; helm
@@ -52,7 +53,10 @@
      go 
      extra-langs
      )
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(
+                                      magithub
+                                      editorconfig
+                                      )
    dotspacemacs-excluded-packages '(
                                     git-gutter
                                     magit-gh-pulls
@@ -152,7 +156,16 @@
 
 (defun dotspacemacs/user-config ()
   (exec-path-from-shell-copy-env "GOPATH")
+   ;; configure editior config
+  (use-package editorconfig
+    :ensure t
+    :config
+    (editorconfig-mode 1))
     )
+  ;; configuration magithub 
+  (use-package magithub
+    :after magit
+    :config (magithub-feature-autoinject t))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -168,3 +181,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (nginx-mode powerline request spinner window-purpose imenu-list parent-mode flx iedit anzu evil goto-chg undo-tree diminish projectile pkg-info epl bind-map bind-key packed async f dash s avy popup package-build smartparens highlight helm helm-core hydra org markdown-mode haml-mode gitignore-mode fringe-helper git-gutter git-gutter+ gh marshal logito pcache ht flyspell-correct flycheck magit magit-popup git-commit with-editor web-completion-data pos-tip go-mode company yasnippet anaconda-mode pythonic auto-complete protobuf-mode wolfram-mode thrift stan-mode scad-mode qml-mode matlab-mode julia-mode arduino-mode wgrep smex slime-company slime ivy-purpose ivy-hydra flyspell-correct-ivy counsel-projectile counsel swiper ivy common-lisp-snippets monokai-theme yapfify yaml-mode xterm-color ws-butler window-numbering which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package toml-mode toc-org tagedit spacemacs-theme spaceline smeargle slim-mode shell-pop scss-mode sass-mode restart-emacs rainbow-mode rainbow-identifiers rainbow-delimiters racer quelpa pyvenv pytest pyenv-mode py-isort pug-mode popwin pip-requirements persp-mode pcre2el paradox orgit org-plus-contrib org-bullets open-junk-file neotree multi-term move-text mmm-mode markdown-toc magit-gitflow magit-gh-pulls macrostep lorem-ipsum livid-mode live-py-mode linum-relative link-hint less-css-mode json-mode js2-refactor js-doc info+ indent-guide ido-vertical-mode ibuffer-projectile hy-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-mode-manager helm-make helm-gtags helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag google-translate golden-ratio go-guru go-eldoc github-search github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gist gh-md ggtags flyspell-correct-helm flycheck-rust flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-commentary evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav dumb-jump disaster diff-hl define-word cython-mode company-web company-tern company-statistics company-quickhelp company-go company-c-headers company-anaconda column-enforce-mode color-identifiers-mode coffee-mode cmake-mode clean-aindent-mode clang-format cargo auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+)
